26ffe6e8fc4d59ed98b27b04965bce57b7d9f4dc
lovent <vuongthai95@gmail.com>
2024-04-04 08:42:10 +0700

    validation

--- controllers/AddController.js
+++ controllers/AddController.js
@@ -1,21 +1,52 @@
 window.AddController = function ($scope,$http,$location) {
     var apiUrl = 'http://localhost:3000/products';
+    var brandUrl = 'http://localhost:3000/brands';
+    
+    $http.get(brandUrl).then(function (res) {
+        $scope.brands = res.data;
+    })
 
     $scope.onSubmit = function () {
-        //lấy dữ liệu từ form
-        var newProduct = {
-            ...$scope.inputValue
+        //khởi tạo biến valid để kiểm tra dữ liệu hợp lệ
+        var valid = true;
+        //kiểm tra trường name
+        if (!$scope.inputValue //không tồn tại inputValue
+            || !$scope.inputValue.name //inputValue.name trống
+            || $scope.inputValue.name.length < 6 //inputValue.name ít hơn 6 ký tự
+            || $scope.inputValue.name.length >100 //inputValue.name nhiều hơn 100 ký tự
+        ) {
+            valid = false;
+        }
+        //kiểm tra trường description
+        if (!$scope.inputValue || !$scope.inputValue.description) {
+            valid = false;
+        }
+        //kiểm tra trường price
+        if (!$scope.inputValue || !$scope.inputValue.price //không tồn tại
+            || isNaN($scope.inputValue.price) //không phải kiểu số
+            || $scope.inputValue.price < 0 //là số âm
+        ) {
+            valid = false;
         }
 
-        $http.post(
-            apiUrl,
-            newProduct
-        ).then(function ($response) {
-            if ($response.status == 201) {
-                $location.path('/');
+        if (valid) {
+            //lấy dữ liệu từ form
+            var newProduct = {
+                ...$scope.inputValue
             }
-        }, function (errors) {
-            console.log(errors);
-        })
+
+            $http.post(
+                apiUrl,
+                newProduct
+            ).then(function ($response) {
+                if ($response.status == 201) {
+                    $location.path('/');
+                }
+            }, function (errors) {
+                console.log(errors);
+            })
+        } else {
+            alert('Dữ liệu không hợp lệ');
+        }
     }
 }
--- controllers/EditController.js
+++ controllers/EditController.js
@@ -16,18 +16,44 @@ window.EditController = function ($scope,$http,$routeParams,$location) {
     $scope.getDetail();
 
     $scope.onUpdate = function () {
-        //lấy dữ liệu từ form
-        var updateProduct = {
-            ...$scope.inputValue
+        //khởi tạo biến valid để kiểm tra dữ liệu hợp lệ
+        var valid = true;
+        //kiểm tra trường name
+        if (!$scope.inputValue //không tồn tại inputValue
+            || !$scope.inputValue.name //inputValue.name trống
+            || $scope.inputValue.name.length < 6 //inputValue.name ít hơn 6 ký tự
+            || $scope.inputValue.name.length >100 //inputValue.name nhiều hơn 100 ký tự
+        ) {
+            valid = false;
+        }
+        //kiểm tra trường description
+        if (!$scope.inputValue || !$scope.inputValue.description) {
+            valid = false;
+        }
+        //kiểm tra trường price
+        if (!$scope.inputValue || !$scope.inputValue.price //không tồn tại
+            || isNaN($scope.inputValue.price) //không phải kiểu số
+            || $scope.inputValue.price < 0 //là số âm
+        ) {
+            valid = false;
         }
 
-        $http.put(
-            `${apiUrl}/${id}`,
-            updateProduct
-        ).then(function (res) {
-            if (res.status == 200) {
-                $location.path('/');
+        if (valid) {
+            //lấy dữ liệu từ form
+            var updateProduct = {
+                ...$scope.inputValue
             }
-        })
+
+            $http.put(
+                `${apiUrl}/${id}`,
+                updateProduct
+            ).then(function (res) {
+                if (res.status == 200) {
+                    $location.path('/');
+                }
+            })
+        } else {
+            alert('Dữ liệu không hợp lệ');
+        }
     }
 }
